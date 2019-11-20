f899a64f6a8b0cf1b969520f9b90030cad7f73cb
Kenneth Andrews <kennethandrews@protonmail.com>
2019-11-20 00:10:54 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
 class Meal
 
+  @@all = []
+
+  attr_accessor :tip, :total
+  attr_reader :waiter, :customer
+
+  def initialize(customer, waiter, total, tip)
+    @customer = customer
+    @waiter = waiter
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,40 @@
 class Waiter
 
+  @@all = []
+
+  attr_accessor :years_of_experience, :meals, :customers
+  attr_reader :name
+
+  def initialize(name, years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience
+    @@all << self
+    @meals = []
+    @customers = [] 
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(customer, self, total, tip)
+  end
+  
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    largest_tip = 0
+    best_tipper = nil
+    self.meals.each do |meal|
+      if meal.tip > largest_tip
+        largest_tip = meal.tip
+        best_tipper = meal.customer
+      end
+    end
+    best_tipper
+  end
+
 end
