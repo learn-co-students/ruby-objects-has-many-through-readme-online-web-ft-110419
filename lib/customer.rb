42e71fc22866908d1592196f9af73e19c7f65922
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-23 17:50:55 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,25 @@
+require "pry"
 class Customer
-  
+attr_reader :name, :age
+attr_accessor :meals
+  @@all = []
+  def initialize(name,age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_meal(waiter,total,tip)
+  self.meals =  Meal.new(waiter,self,total,tip)
+
+  end
+  def meals
+    customer_meals = Meal.all.find_all {|meal| meal.customer == self}
+  end
+  def waiters
+     meals.map { |meal| meal.waiter}
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,25 @@
 class Waiter
+    attr_reader :name, :experience
+    @@all = []
+  def initialize(name,experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def waiter
+    @@all[0]
+  end
+  def new_meal(customer,total,tip)
+    Meal.new(self,customer,total,tip)
+  end
+  def meals
+    Meal.all.find_all {|meal| meal.waiter == self}
+  end
+  def best_tipper
+   meals.sort { |a,b| b.tip <=> a.tip}.first.customer
 
+  end
 end
